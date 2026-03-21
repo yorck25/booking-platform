6bb239391073538b6d3630fef5d20a3586b11a32
yorck25 <100796096+yorck25@users.noreply.github.com>
2026-03-21 23:43:59 +0100

    #2: * adds select and divider component

---
+++ apps/booking-client/src/components/divider/index.tsx
@@ -0,0 +1,11 @@
+export const Divider = () => {
+    const styles = {
+        "height": "1px",
+        "width": "100%",
+        "background": "#DEE2E6"
+    }
+
+    return (
+        <div style={styles}/>
+    )
+}
