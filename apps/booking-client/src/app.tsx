90e6e9294b82cfaaa18e052534b5a1ae092fc6b5
yorck25 <100796096+yorck25@users.noreply.github.com>
2026-03-29 15:02:39 +0200

    #2: * adds router and all routes * adjust max-width for desktop

--- apps/booking-client/src/app.tsx
+++ apps/booking-client/src/app.tsx
@@ -1,12 +1,37 @@
 import './app.css'
 import {ServicePage} from "./routes/servicePage";
+import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
+import {ReservationPage} from "./routes/reservationPage";
+import {PersonalDataPage} from "./routes/personalDataPage";
+import {useEffect} from "preact/hooks";
+import {SuccessBookingView} from "./routes/successBookingView";
 
 export function App() {
-
-
     return (
         <>
-            <ServicePage/>
+            <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<RedirectToServices />} />                        <Route path="/services" element={<ServicePage />} />
+                        <Route path="/reservation" element={<ReservationPage />} />
+                        <Route path="/personal-data" element={<PersonalDataPage />} />
+                        <Route path="/success-booking" element={<SuccessBookingView />} />
+                    </Routes>
+            </BrowserRouter>
         </>
     );
 }
+
+const RedirectToServices = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate("/services");
+    }, []);
+
+    return (
+        <>
+            <p>Redirecting to services...</p>
+            <a href="/services">Click here if not redirected</a>
+        </>
+    );
+};
